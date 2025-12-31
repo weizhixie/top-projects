7d127762ec2ff6cf5f7d646aa8cf89412c3a2160
weizhixie <wx02697@yahoo.co.uk>
2025-12-31 21:04:26 +0000

    Add main.js and append method

--- linkedLists/linkedList.js
+++ linkedLists/linkedList.js
@@ -4,4 +4,16 @@ export class LinkedList {
   constructor() {
     this.headNode = null;
   }
+
+  append(value) {
+    if (this.headNode === null) {
+      this.headNode = new Node(value);
+    } else {
+      let temp = this.headNode;
+      while (temp.nextNode !== null) {
+        temp = temp.nextNode;
+      }
+      temp.nextNode = new Node(value);
+    }
+  }
 }
