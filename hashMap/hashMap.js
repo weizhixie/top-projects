70809ce6278c34674033f66df2accf14bd0d7bc1
weizhixie <wx02697@yahoo.co.uk>
2026-01-03 18:58:12 +0000

    Implement getBucket and set method

--- hashMap/hashMap.js
+++ hashMap/hashMap.js
@@ -2,6 +2,7 @@ export class HashMap {
   constructor() {
     this.capacity = 16;
     this.loadFactor = 0.75;
+    this.buckets = Array.from({ length: this.capacity }, () => []);
   }
 
   hash(key) {
@@ -13,5 +14,28 @@ export class HashMap {
     }
 
     return hashCode;
+  }
+
+  getBucket(key) {
+    const index = this.hash(key);
+
+    if (index < 0 || index >= this.buckets.length) {
+      throw new Error("Trying to access index out of bounds");
+    }
+
+    return this.buckets[index];
+  }
+
+  set(key, value) {
+    const bucket = this.getBucket(key);
+
+    for (const item of bucket) {
+      if (item.key === key) {
+        item.value = value;
+        return;
+      }
+    }
+
+    bucket.push({ key, value });
   }
 }
