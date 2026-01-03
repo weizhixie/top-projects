596e5a05e2f1e6538c07fd791d7825e268306410
weizhixie <wx02697@yahoo.co.uk>
2026-01-03 15:35:48 +0000

    Implement hash method

--- hashMap/hashMap.js
+++ hashMap/hashMap.js
@@ -3,4 +3,15 @@ export class HashMap {
     this.capacity = 16;
     this.loadFactor = 0.75;
   }
+
+  hash(key) {
+    let hashCode = 0;
+    const primeNumber = 31;
+
+    for (let i = 0; i < key.length; i++) {
+      hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.capacity;
+    }
+
+    return hashCode;
+  }
 }
