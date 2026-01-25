dc5262115a767e21417069996a42f55eb5a56d07
weizhixie <wx02697@yahoo.co.uk>
2026-01-25 22:18:34 +0000

    Add driver script to test BST

--- binarySearchTree/index.js
+++ binarySearchTree/index.js
@@ -1,6 +1,14 @@
 import { Tree } from "./tree.js";
 
-const testTree = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
+const getRandArr = () => {
+  const randArr = [];
+  for (let index = 0; index < 15; index++) {
+    randArr.push(Math.floor(Math.random() * 100));
+  }
+  return randArr;
+};
+
+const testTree = new Tree(getRandArr());
 
 const prettyPrint = (node, prefix = "", isLeft = true) => {
   if (node === null) {
@@ -16,3 +24,41 @@ const prettyPrint = (node, prefix = "", isLeft = true) => {
 };
 
 prettyPrint(testTree.root);
+console.log(`Is balanced: ${testTree.isBalanced()}`);
+
+const levelOrder = [];
+const inOrder = [];
+const preOrder = [];
+const postOrder = [];
+testTree.levelOrderForEachUsingIteration((node) => levelOrder.push(node.data));
+testTree.inOrderForEach((node) => inOrder.push(node.data));
+testTree.preOrderForEach((node) => preOrder.push(node.data));
+testTree.postOrderForEach((node) => postOrder.push(node.data));
+console.log(`Level Order: ${levelOrder}`);
+console.log(`In Order: ${inOrder}`);
+console.log(`Pre-order: ${preOrder}`);
+console.log(`Post-order: ${postOrder}`);
+
+testTree.insert(321);
+testTree.insert(132);
+testTree.insert(323);
+testTree.insert(1234);
+testTree.insert(213);
+prettyPrint(testTree.root);
+console.log(`Is balanced: ${testTree.isBalanced()}`);
+testTree.rebalance();
+prettyPrint(testTree.root);
+console.log(testTree.isBalanced());
+
+levelOrder.length = 0;
+inOrder.length = 0;
+preOrder.length = 0;
+postOrder.length = 0;
+testTree.levelOrderForEachUsingIteration((node) => levelOrder.push(node.data));
+testTree.inOrderForEach((node) => inOrder.push(node.data));
+testTree.preOrderForEach((node) => preOrder.push(node.data));
+testTree.postOrderForEach((node) => postOrder.push(node.data));
+console.log(`Level Order: ${levelOrder}`);
+console.log(`In Order: ${inOrder}`);
+console.log(`Pre-order: ${preOrder}`);
+console.log(`Post-order: ${postOrder}`);
